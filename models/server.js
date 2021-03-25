22209d0c4fe7ec9ef3bb35bee8ad2304a914a60d
elmike1603@gmail.com <elmike1603@gmail.com>
2021-03-24 21:51:00 -0500

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const cors = require('cors');
+
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
+
+        //Middleware
+        this.middlewares();
+
+
+
+        //Routes
+        this.routes();
+    }
+
+
+    middlewares() {
+
+        this.app.use(cors());
+
+        this.app.use(express.json());
+
+        this.app.use(express.static('public'));
+
+    }
+
+    routes() {
+        this.app.use(this.usuariosPath, require('../routes/usuarios'));
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor running on port:', this.port);
+        });
+    }
+}
+
+module.exports = Server;
